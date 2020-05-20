0c2f54d31baf7510ea9ed11290b9e33fc9d3e001
Nuru-Shi <63554453+Nuru-Shi@users.noreply.github.com>
2020-05-20 23:26:50 +0200

    Add files via upload

---
+++ Python Excersises/tuple_list_input/tuplelistinput.py
@@ -0,0 +1,6 @@
+num = input("Give me an array of numbers and i will turn it into a list and tuple to print them for you!\n")
+
+numlist = [num]
+numtuple = (num)
+
+print(str(numlist) + str(numtuple))
